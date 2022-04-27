1703606412d413b91916fa4c68e27d5db433d0cd
willisit12 <willisit22@gmail.com>
2022-04-27 05:06:41 +0000

    Add your commit

---
+++ processWebHook.py
@@ -0,0 +1,15 @@
+import flask
+import os
+from flask import send_from_directory
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return "Hello World"
+
+if __name__ == "__main__":
+    app.secret_key = 'ItIsASecret'
+    app.debug = True
+    app.run()
